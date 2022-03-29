aec057b3ced6ec3999bd6267101b61d34d7f2b80
jaewoo kim <rlawodn0510@yahoo.co.jp>
2022-03-29 21:27:46 +0900

    MyBatis_Test  first Commit

---
+++ MyBatis/src/main/java/yuki/spring/web/board/BoardServiceClient.java
@@ -0,0 +1,22 @@
+package yuki.spring.web.board;
+
+import java.util.List;
+import yuki.spring.web.board.impl.BoardDAO;
+
+public class BoardServiceClient {
+	public static void main(String[] args) {
+		BoardDAO boardDAO = new BoardDAO();
+		BoardVO vo = new BoardVO();
+		vo.setTitle("myBatis 테스트");
+		vo.setWriter("YUKI");
+		vo.setContent("myBatis 내용");
+		boardDAO.insertBoard(vo);
+		vo.setSearchCondition("TITLE");
+		vo.setSearchKeyword("");
+		List<BoardVO> boardList = boardDAO.getBoardList(vo);
+		for (BoardVO board : boardList) {
+			System.out.println("===> " + board.toString());
+		}
+	}
+
+}
